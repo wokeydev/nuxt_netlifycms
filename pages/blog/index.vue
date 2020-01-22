8640dfdc65d9be37185c2fa1387e6f32c92a5c12
fairy <whitedeveloper0203@mail.ru>
2020-01-23 04:25:56 +1000

    deploy

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,6 +1,12 @@
 <template>
   <article>
-      {{blogPosts}}
+        <ul id="v-for-object" class="demo">
+            <li v-for="value in blogPosts">
+                <h2>{{ value.title }}</h2>
+                <h5>{{ value.description }}</h5>
+                <h5>{{ value.body }}</h5>
+            </li>
+        </ul>
   </article>
 </template>
 
