bfc02f458a2e5460c662c8f2660dad71d6473b3e
fairy <whitedeveloper0203@mail.ru>
2020-01-23 02:38:00 +1000

    deploy

--- pages/index.vue
+++ pages/index.vue
@@ -32,6 +32,11 @@
 import Logo from '~/components/Logo.vue'
 
 export default {
+  head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    };
+  },
   components: {
     Logo
   }
